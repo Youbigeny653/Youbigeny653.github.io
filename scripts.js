df6938a2c8aba214694e649ca9e24eb0d7a5002f
Youbigeny65@ <gyoubi.ir2024@esaip.org>
2025-11-06 01:04:10 +0100

    Page setup with index code, project code and style

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
+// Placeholder for calling your live API (to be filled in later)
+
+console.log("Portfolio loaded. Ready for API integration!");
+
+// Example fetch call (to replace later)
+// fetch("https://your-api-url.com/data")
+//  .then(res => res.json())
+//  .then(json => console.log(json));
 
